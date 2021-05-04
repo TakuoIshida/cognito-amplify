cf59320ea6cd4a696382a7b2934257dcde5b374d
takuo_ishida <archersparadox.100@gmail.com>
2021-05-04 23:59:57 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { AmplifyAuthenticator, AmplifySignOut, AmplifySignUp } from '@aws-amplify/ui-react';
+import Amplify, { I18n } from 'aws-amplify';
+import React from 'react';
 import './App.css';
+import { cognitoConstants } from './constants/auth';
+import { vocabularies } from './constants/i18n';
+Amplify.configure(cognitoConstants);
+I18n.putVocabularies(vocabularies);
+I18n.setLanguage('ja');
 
-function App() {
-  return (
+const AuthStateApp = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+      return onAuthUIStateChange((nextAuthState, authData) => {
+          setAuthState(nextAuthState);
+          setUser(authData)
+      });
+  }, []);
+
+return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>Hello, {user.username}</div>
+        <AmplifySignOut />
     </div>
-  );
+) : (
+  <AmplifyAuthenticator>
+  <AmplifySignUp
+    slot="sign-up"
+    formFields={[
+      { type: "username" },
+      { type: "password" },
+      { type: "email" },
+    ]}
+  />
+</AmplifyAuthenticator>
+);
 }
 
-export default App;
+export default AuthStateApp;
